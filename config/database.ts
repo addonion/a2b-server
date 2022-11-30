200c0cdd17b10c22da7932d1e4150934c443ea7e
addonion <addonion@gmail.com>
2022-11-30 07:52:46 +0600

    Change db

--- config/database.ts
+++ config/database.ts
@@ -2,12 +2,12 @@ export default ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', ''),
-      port: env.int('DATABASE_PORT', 6472),
-      database: env('DATABASE_NAME', 'railway'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', ''),
-      ssl: env.bool('DATABASE_SSL', false),
+      host: env("DATABASE_HOST", ""),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "postgres"),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", ""),
+      ssl: env.bool("DATABASE_SSL", false),
     },
   },
 });
